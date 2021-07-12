6bc9411ae700dd8ef4d7261a777e8639e0eaac68
Nuwanda <morato_dev@protonmail.com>
2021-07-12 20:39:59 -0300

    requisito 05 finalizado itens ja estao renderizados na tela

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class SearchBar extends React.Component {
   render() {
@@ -10,11 +11,24 @@ export default class SearchBar extends React.Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <div>
-          {
-            console.log(itemList)
-          }
+          {itemList.map((item) => (
+            <div
+              style={ { border: '1px solid black' } }
+              data-testid="product"
+              key={ item.id }
+            >
+              <h3>{item.title}</h3>
+              <img src={ item.thumbnail } alt="Foto do Produto" />
+              <p>{item.price}</p>
+            </div>
+          ))}
         </div>
       </div>
     );
   }
 }
+
+SearchBar.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  itemList: PropTypes.arrayOf(Object).isRequired,
+};
