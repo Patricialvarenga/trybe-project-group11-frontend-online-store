bd44801868eb410aac901cafcc8d56a016904903
Nuwanda <morato_dev@protonmail.com>
2021-07-12 16:58:52 -0300

    requisito 1 funçao getcategories feito

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Iniciando projeto
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>Edit src/App.js and save to reload.</p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Iniciando projeto
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 export async function getCategories() {
-  // Implemente aqui
+  // "https://api.mercadolibre.com/sites/MLB/categories"
+  const fetchCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categoriesJson = await fetchCategories.json();
+  return categoriesJson;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
