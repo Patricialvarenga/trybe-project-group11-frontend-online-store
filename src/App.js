e0e167fe778cd92a03b31fd4eeeecc3b76eb2a2d
Nuwanda <morato_dev@protonmail.com>
2021-07-12 17:16:52 -0300

    requisito 2 componente SearchBar feito

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>Edit src/App.js and save to reload.</p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Iniciando projeto
-          </a>
-        </header>
+      <div>
+        <SearchBar />
       </div>
     );
   }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
