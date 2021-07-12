078f8fde7362af9610821d3e324fcc346e429a04
Nuwanda <morato_dev@protonmail.com>
2021-07-12 16:30:25 -0300

    Iniciando projeto

--- src/App.js
+++ src/App.js
@@ -8,13 +8,8 @@ function App() {
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Iniciando projeto
         </a>
       </header>
     </div>
