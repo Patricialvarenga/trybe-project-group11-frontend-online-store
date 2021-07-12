ff7291edcb3ca2564b3b7482c02ce98394c6c15a
Nuwanda <morato_dev@protonmail.com>
2021-07-12 20:31:15 -0300

    parte do state e fetch do requisito 05 feito

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import './App.css';
-import SearchBar from './components/SearchBar';
 import ShoppingCart from './components/ShoppingCart';
 import CategoriesList from './components/CategoriesList';
+import ItemsList from './components/ItemsList';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <BrowserRouter>
-          <Route exact path="/" component={ SearchBar } />
+          <Route exact path="/" component={ ItemsList } />
           <Route exact path="/" component={ CategoriesList } />
           <Link data-testid="shopping-cart-button" to="/shoppingcart">
             Carrinho de Compras
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 export default class SearchBar extends React.Component {
   render() {
+    const { handleChange, itemList } = this.props;
     return (
       <div>
-        <input type="text" />
+        <input type="text" data-testid="query-input" onChange={ handleChange } />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <div>
+          {
+            console.log(itemList)
+          }
+        </div>
       </div>
     );
   }
