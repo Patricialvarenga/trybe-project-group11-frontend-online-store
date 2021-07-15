84ece2f62e7940f4de9930673291f2010d2c8756
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-07-15 18:26:52 -0300

    requisito 09

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { IoCartOutline } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
 import { getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class ProductDetails extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.fetchProduct = this.fetchProduct.bind(this);
+    this.addItemCart = this.addItemCart.bind(this);
 
     this.state = {
       produto: [],
+      qtdCart: 0,
     };
   }
 
   componentDidMount() {
     this.fetchProduct();
+  }
+
+  addItemCart(produto) {
+    console.log(produto);
+    const listCart = localStorage
+      .getItem('itensCart') ? JSON.parse(localStorage.getItem('itensCart')) : [];
+    listCart.push(produto);
+    localStorage.setItem('itensCart', JSON.stringify(listCart));
+    this.setState({
+      qtdCart: listCart.length,
+    });
   }
 
   async fetchProduct() {
@@ -31,13 +45,36 @@ export default class ProductDetails extends React.Component {
   }
 
   render() {
-    const { produto } = this.state;
+    const { produto, qtdCart } = this.state;
     return (
       <div>
-        <h3 data-testid="product-detail-name">{produto.title}</h3>
-        <img src={ produto.thumbnail } alt="Foto do produto selecionado" />
-
+        <section>
+          <Link to="/">Home</Link>
+          <Link to="/shoppingcart" data-testid="shopping-cart-button">
+            <IoCartOutline size={ 32 } />
+          </Link>
+          <span>
+            { qtdCart }
+          </span>
+          <div>
+            <h3 data-testid="product-detail-name">{produto.title}</h3>
+            <img src={ produto.thumbnail } alt="Foto do produto selecionado" />
+            <p>
+              R$
+              {produto.price}
+            </p>
+            <button
+              type="button"
+              onClick={ (
+              ) => this.addItemCart(produto) }
+              data-testid="product-detail-add-to-cart"
+            >
+              Adicionar ao carrinho
+            </button>
+          </div>
+        </section>
       </div>
+
     );
   }
 }
